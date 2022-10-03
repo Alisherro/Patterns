7f6aecaaa0a5607ad41c82f337130d34af3b9bc2
Alisherro <94703417+Alisherro@users.noreply.github.com>
2022-10-03 23:44:37 +0600

    Create decorator2.dart

---
+++ decorator2.dart
@@ -0,0 +1,49 @@
+abstract class Car {
+  void assemble();
+}
+
+class BasicCar implements Car {
+  void assemble() {
+    print('Basic Car');
+  }
+}
+
+class CarDecorator implements Car {
+  late Car car;
+
+  CarDecorator(this.car);
+  void assemble() {
+    this.car.assemble();
+  }
+}
+
+class SportsCar extends CarDecorator {
+  SportsCar(Car car) : super(car);
+
+  void assemble() {
+    super.assemble();
+    print(" Adding features of Sports Car.");
+  }
+}
+
+class LuxuryCar extends CarDecorator {
+  LuxuryCar(Car car) : super(car);
+
+  create(Car c) {
+    CarDecorator(c);
+  }
+
+  void assemble() {
+    super.assemble();
+    print(" Adding features of Luxury Car.");
+  }
+}
+
+void main() {
+  Car sportsCar = new SportsCar(new BasicCar());
+  sportsCar.assemble();
+  print("\n*****");
+
+  Car sportsLuxuryCar = new SportsCar(new LuxuryCar(new BasicCar()));
+  sportsLuxuryCar.assemble();
+}
