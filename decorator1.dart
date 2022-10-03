79cc90a006b86142f1866d58ae087cc487277824
Alisherro <94703417+Alisherro@users.noreply.github.com>
2022-10-03 23:24:44 +0600

    Update decorator1.dart

--- decorator1.dart
+++ decorator1.dart
@@ -1,58 +1,48 @@
-abstract class Beverage {
-  double get cost;
-  String get ingredients;
+abstract class Shape {
+  String draw();
 }
 
-class Ingredient {
-  double cost;
-  String name;
-
-  Ingredient(this.name, this.cost);
-
-  @override
-  String toString() => name;
+class Square implements Shape {
+  String draw() => "Square";
 }
 
-var coffee = Ingredient("coffee", .25);
-var milk = Ingredient("milk", .5);
-var sugar = Ingredient("sugar", .1);
-
-class Coffee implements Beverage {
-  final Set<Ingredient> _ingredients = {coffee, milk, sugar};
-
-  @override
-  double get cost =>
-      _ingredients.fold(0, (total, ingredient) => total + ingredient.cost);
-
-  @override
-  String get ingredients {
-    var stringIngredients = _ingredients.fold<String>("", (total, ingredient) {
-      if (_ingredients.last.name == ingredient.name) {
-        return total + "and ${ingredient.name}";
-      }
-      return total + "${ingredient.name}, ";
-    });
-
-    return stringIngredients;
-  }
+class Triangle implements Shape {
+  String draw() => "Triangle";
 }
 
-class StarbucksCoffeeDecorator implements Beverage {
-  final Beverage _coffee = Coffee();
+abstract class ShapeDecorator implements Shape {
+  final Shape shape;
+
+  ShapeDecorator(this.shape);
+
+  String draw();
+}
+
+class GreenShapeDecorator extends ShapeDecorator {
+  GreenShapeDecorator(Shape shape) : super(shape);
 
   @override
-  double get cost => _coffee.cost * 5;
+  String draw() => "Green ${shape.draw()}";
+}
+
+class BlueShapeDecorator extends ShapeDecorator {
+  BlueShapeDecorator(Shape shape) : super(shape);
 
   @override
-  String get ingredients => _coffee.ingredients;
+  String draw() => "Blue ${shape.draw()}";
 }
 
 void main() {
-  var coffee = Coffee();
-  var starbucksCoffee = StarbucksCoffeeDecorator();
+  final square = Square();
+  print(square.draw());
 
-  print("Coffee contains ${coffee.ingredients}. It costs \$${coffee.cost}");
-  print(
-      "Starbucks coffee contains ${starbucksCoffee.ingredients}. It costs \{starbucksCoffee.cost}");
+  final greenSquare = GreenShapeDecorator(square);
+  print(greenSquare.draw());
 
+  final blueGreenSquare = BlueShapeDecorator(greenSquare);
+  print(blueGreenSquare.draw());
+
+  final blueGreenGreenGreenGreenSquare = GreenShapeDecorator(BlueShapeDecorator(GreenShapeDecorator(
+      GreenShapeDecorator(GreenShapeDecorator(GreenShapeDecorator(square))))));
+  print(blueGreenGreenGreenGreenSquare.draw());
 }
